f48687dceae721ee3317471be0654bc6082d5787
Aabha <aabha.khandwala@gmail.com>
2020-05-09 23:22:54 -0400

    added .md with answers

--- NestedLoop.java
+++ NestedLoop.java
@@ -1,7 +1,7 @@
 public class NestedLoop {
     public static void main(String[] args) {
         // this is #1 - I'll call it "CN"
-        // inner loop changes faster tahn outer loop.
+        // ans 1: inner loop changes faster than outer loop.
         for (char c = 'A'; c <= 'E'; c++) {
             for (int n = 1; n <= 3; n++) {
                 System.out.println(c + " " + n);
@@ -14,6 +14,7 @@ public class NestedLoop {
         for (int a = 1; a <= 3; a++) {
             for (int b = 1; b <= 3; b++) {
                 System.out.print(a + "-" + b + " ");
+                // ans:3 System.out.println(a + "-" + b + " ");
             }
             // * You will add a line of code here.
             // System.out.println(a + "-" + b + " ");// b is out of scope here, outside of
@@ -22,6 +23,7 @@ public class NestedLoop {
 
         System.out.println("\n");
 
+        // ans:2 by inter changing inner outer loop
         for (int n = 1; n <= 3; n++) {
             for (char c = 'A'; c <= 'E'; c++) {
                 System.out.println(n + " " + c);
---
+++ NestedLoop.md
@@ -0,0 +1,11 @@
+Look at the first set of nested loops ("CN"). Which variable changes faster? Is it the variable controlled by the outer loop (c) or the variable controlled by the inner loop (n)? Answer in a comment.
+Ans: inner loop changes faster than outer loop.variable controlled by the inner loop(n).
+
+Change the order of the loops so that the "c" loop is on the inside and the "n" loop is on the outside. How does the output change?
+Ans: 1A1V1C2A2B2C3A3B3C
+
+Look at the second set of nested loops ("AB"). Change the print() statement to println(). How does the output change? (Then change it back to print().)
+Ans:print() will print output in same line.while println will print output in newline.
+
+Add a System.out.println() statement after the close brace of the inner loop (the "b" loop), but still inside the outer loop. How does the output change?
+Ans:it will throw error as b is out of scope here, outside of the loop.
